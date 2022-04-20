31bdc9f30d76a4ca55cb6345dce8b205b833c06f
Sabah Shaikh <smshaikh@ucsc.edu>
2022-04-20 13:33:07 -0700

    lab5
    lavascript and html

---
+++ lab5/js/lab.js
@@ -0,0 +1,22 @@
+/*
+*Author: Sabah Shaikh
+*Created: 20 april 2022
+*License: Public Domain
+*/
+
+// Define Variables
+make = "subaru";
+model = "forester";
+color = "silver";
+year = 2015;
+ownIt = true;
+
+//Calculate
+age = 2022 - year;
+
+//Output
+document.writeln("Make: " + make + "<br>");
+document.writeln("Model: " + model + "<br>");
+document.writeln("Color: " + color + "<br>");
+document.writeln("Year: " + year + "<br>");
+document.writeln("Age: " + age + "years<br>");
