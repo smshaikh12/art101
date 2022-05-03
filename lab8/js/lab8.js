06ddfda87a9d992b0de6a6e914fe2cd6a594f795
Sabah Shaikh <smshaikh@ucsc.edu>
2022-05-02 19:29:16 -0700

    lab8
    no

---
+++ lab8/js/lab8.js
@@ -0,0 +1,33 @@
+/*
+*Author: Sabah Shaikh
+*Created: 25 April 2022
+*License: Public Domain
+*/
+
+/*
+* Author: Cassidy Crawford
+* Created: April 29th
+* License: Public Domain
+*/
+
+function addTwo(x){
+  return (x + 2);
+}
+
+// test function
+console.log(addTwo(5));
+
+console.log(addTwo(20));
+
+aNumberArray = [22, 45, 99]
+console.log("My Array", aNumberArray);
+
+var result = aNumberArray.map(addTwo);
+// should return with two added to each number in the array
+console.log("add two to every number in aNumberArray", result)
+
+var result = aNumberArray.map(function(x){
+    return x * 10;
+})
+//should return the array mutiplied by ten [220, 450, 990]
+console.log("mutiple each number in aNumberArray by ten", result)
