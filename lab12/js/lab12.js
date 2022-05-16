d485b1a0301ea943874456ab45ecef5c9b81ac19
Sabah Shaikh <smshaikh@ucsc.edu>
2022-05-16 10:27:08 -0700

    lab12
    sorting houses

---
+++ lab12/js/lab12.js
@@ -0,0 +1,32 @@
+/*
+*Author: Sabah Shaikh
+*Created: 16 May 2022
+*License: Public Domain
+*/
+
+//return Results
+//depending on length mode 4
+function sortingFlower (str) {
+length = str.length;
+mod = length % 4;
+if (mod == 0) {
+  return "Rose"
+}
+else if (mod == 1) {
+  return "Daisy"
+}
+else if (mod == 2) {
+  return "Sunflower"
+}
+else if (mod == 3) {
+  return "Tulip"
+  }
+}
+
+var myButton = document.getElementById("button");
+myButton.addEventListener("click", function()  {
+  var  name = document.getElementById("input").value;
+  var flower = sortingFlower(name);
+  newText = "<p> you have been sorted into: " + flower +"</p>";
+  document.getElementById("output").innerHTML = newText;
+})
