f2f10db6e3d4509a9d1976f0d12bfaaf5da1bc4f
Sabah Shaikh <smshaikh@ucsc.edu>
2022-05-25 11:44:17 -0700

    lab15

---
+++ lab15/js/lab15.js
@@ -0,0 +1,30 @@
+/*
+*Author: Sabah Shaikh
+*Created: 25 May 2022
+*License: Public Domain
+*/
+var endpoint = "https://yesno.wtf/api";
+
+function addText(text) {
+$("#output").html(text);
+}
+
+function getAjax() {
+	$.ajax({
+  	url: endpoint,
+    type: "GET"
+  })
+
+  .done(function(data){
+  	console.log("Worked!");
+    str = ""
+    str += "<img src=" + data.image + ">"
+    str += "<p>" + data.answer + "<p>";
+  	addText(str);
+  })
+  .fail(function(request, error){
+  	$("#output").html("error");
+  })
+}
+
+$("#activate").click(getAjax);
